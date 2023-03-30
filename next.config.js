c4b7c6a7389500e254f94ec0fca48d68ef5aff14
Peerawit Wiboonthanakun <Peerawit2000@gmail.com>
2023-03-30 21:24:12 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 module.exports = {
+  env: {
+    REACT_APP_URL: "http://localhost:5174",
+  },
   async redirects() {
     return [
       {

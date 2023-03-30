dd9527930e8da4a1abebbe5c6d6f5de1e062d316
Peerawit Wiboonthanakun <Peerawit2000@gmail.com>
2023-03-30 21:24:57 +0700

    connect api select_branch

--- src/dataService/api_branch/get.ts
+++ src/dataService/api_branch/get.ts
@@ -1,7 +1,9 @@
+import axios from "axios";
+
 export type ITypeBranch = {
   branch_id: number;
   branch_name: string;
-  is_active: string;
+  active: string;
   create_dt: string;
 };
 
@@ -9,27 +11,7 @@ export type ITypeBranchBody = {
   result: ITypeBranch[];
 };
 
-const mockData: ITypeBranch[] = [
-  {
-    branch_id: 1,
-    branch_name: "ลาดพร้าว71",
-    is_active: "y",
-    create_dt: "2022-01-01 11:00",
-  },
-  {
-    branch_id: 2,
-    branch_name: "ลาดพร้าว78",
-    is_active: "y",
-    create_dt: "2022-01-01 11:00",
-  },
-  {
-    branch_id: 3,
-    branch_name: "ลาดกระบัง",
-    is_active: "y",
-    create_dt: "2022-01-01 11:00",
-  },
-];
-
-export function getTypeBranch(): Promise<ITypeBranchBody> {
-  return Promise.resolve({ result: mockData });
+export async function getTypeBranch(): Promise<ITypeBranchBody> {
+  const result = await axios.get(`${process.env.REACT_APP_URL}/api/branch`);
+  return Promise.resolve({ result: result.data.result });
 }
